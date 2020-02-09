f4b8b3d4dfab57c4e9c5645ac31e98b2f491b041
Matt Cuento <cuentomr17@gmail.com>
2020-02-08 17:55:12 -0800

    Added db file

---
+++ cortisol_app/cortisol_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cortisol_app
+//
+//  Created by jamie jenkins on 2/8/20.
+//  Copyright © 2020 Rolling Thunder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
